bf5eb64822d060bf17e6a912c69947a8240e85cc
Vojtěch Kolenčík <kolencik@gmail.com>
2018-08-26 23:53:40 +0200

    Add middleware libs to project

--- project.clj
+++ project.clj
@@ -1,10 +1,15 @@
 (defproject clojure-rest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "REST service for documents"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.1.2"]
+                 [c3p0/c3p0 "0.9.1.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.168"]
+                 [cheshire "4.0.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clojure-rest.handler/app}
   :profiles
